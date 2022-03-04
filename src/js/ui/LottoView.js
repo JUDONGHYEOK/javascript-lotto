f663b5d02770d3fee9c3152467384022806fc409
JUDONGHYEOK <judonghyeok@gmail.com>
2022-03-04 22:40:47 +0900

    refactor: LottoView 2번 이상 사용하는 element 상수화

--- src/js/ui/LottoView.js
+++ src/js/ui/LottoView.js
@@ -12,9 +12,14 @@ import { LottoModal } from './LottoModal.js';
 export default class LottoView {
   constructor() {
     this.machine = new LottoMachine();
+    this.lottoModal = new LottoModal(this);
     this.$lottoResultContainer = $(DOM.ID.LOTTO_RESULT_CONTAINER);
+    this.$winningNumberInputArr = document.querySelectorAll(
+      '.winning-number-input'
+    );
+    this.$purchaseMoneyInput = $(DOM.ID.PURCHASE_MONEY_INPUT);
+    this.$purchaseMoneyButton = $(DOM.ID.PURCHASE_MONEY_BUTTON);
     this.bindEvents();
-    this.lottoModal = new LottoModal(this, this.machine);
   }
 
   bindEvents() {
@@ -81,8 +86,8 @@ export default class LottoView {
   }
 
   disablePurchase() {
-    $(DOM.ID.PURCHASE_MONEY_INPUT).disabled = true;
-    $(DOM.ID.PURCHASE_MONEY_BUTTON).disabled = true;
+    this.$purchaseMoneyInput.disabled = true;
+    this.$purchaseMoneyButton.disabled = true;
   }
 
   showLottoContainers() {
@@ -96,7 +101,7 @@ export default class LottoView {
   }
 
   focusWinningNumberForm() {
-    document.querySelector('.winning-number-input').focus();
+    this.$winningNumberInputArr[0].focus();
   }
 
   bindEventsToResultForm() {
@@ -108,9 +113,9 @@ export default class LottoView {
 
   handleResultForm(e) {
     e.preventDefault();
-    const winningNumbers = Array.from(
-      document.querySelectorAll('.winning-number-input')
-    ).map(({ value }) => Number.parseInt(value));
+    const winningNumbers = Array.from(this.$winningNumberInputArr).map(
+      ({ value }) => Number.parseInt(value)
+    );
     try {
       validateArrayNumber(winningNumbers);
       const bonusNumber = winningNumbers.pop();
@@ -128,12 +133,10 @@ export default class LottoView {
   }
 
   reactivatePurchaseForm() {
-    document
-      .querySelectorAll('.winning-number-input')
-      .forEach((element) => (element.value = ''));
-    $('purchase-money-input').value = '';
-    $('purchase-money-input').disabled = false;
-    $('purchase-money-button').disabled = false;
+    this.$winningNumberInputArr.forEach((element) => (element.value = ''));
+    this.$purchaseMoneyInput.value = '';
+    this.$purchaseMoneyInput.disabled = false;
+    this.$purchaseMoneyButton.disabled = false;
     $(DOM.ID.TOGGLE_CHECKBOX).checked = false;
   }
 }

8b5b3b9e8c758a21a1d18a43d37d850a0189f5a8
JUDONGHYEOK <judonghyeok@gmail.com>
2022-03-04 18:35:15 +0900

    feat: LottoView 모달 종료기능 추가

--- src/js/ui/LottoView.js
+++ src/js/ui/LottoView.js
@@ -87,6 +87,11 @@ export default class LottoView {
     $(DOM.ID.WINNING_NUMBER_FORM).hidden = false;
   }
 
+  hideLottoContainers() {
+    $(DOM.ID.LOTTO_RESULT_SECTION).hidden = true;
+    $(DOM.ID.WINNING_NUMBER_FORM).hidden = true;
+  }
+
   focusWinningNumberForm() {
     document.querySelector('.winning-number-input').focus();
   }
@@ -127,5 +132,10 @@ export default class LottoView {
  `
     );
     $('modal').style.display = 'flex';
+    $('modal-close').addEventListener('click', this.closeModal.bind(this));
+    $('restart').addEventListener('click', this.restart.bind(this));
+  }
+  closeModal() {
+    $('modal').style.display = 'none';
   }
 }

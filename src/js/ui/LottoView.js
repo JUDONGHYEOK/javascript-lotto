8c226e499c846d380381f0741c621672b88d548f
JUDONGHYEOK <judonghyeok@gmail.com>
2022-03-04 21:00:23 +0900

    feat: 수익률 계산 기능 추가

--- src/js/ui/LottoView.js
+++ src/js/ui/LottoView.js
@@ -135,6 +135,9 @@ export default class LottoView {
     )}개</span></div>
  `
       );
+      $(
+        'lotto-result-rate'
+      ).textContent = `당신의 총 수익률은 ${this.machine.profitRate}%입니다.`;
       $('modal').style.display = 'flex';
       $('modal-close').addEventListener('click', this.closeModal.bind(this));
       $('restart').addEventListener('click', this.restart.bind(this));
@@ -160,5 +163,6 @@ export default class LottoView {
     $('purchase-money-input').value = '';
     $('purchase-money-input').disabled = false;
     $('purchase-money-button').disabled = false;
+    $(DOM.ID.TOGGLE_CHECKBOX).checked = false;
   }
 }
